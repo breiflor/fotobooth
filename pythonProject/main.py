1453b55cc39efd92632f15cdf50be5afd444cc13
MrBreitkopf <git@mrbreitkopf.de>
2024-05-11 14:33:48 +0200

    implemented pygame again as the preeview is now way more responsive

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -4,7 +4,9 @@ import cv2
 import numpy as np
 import datetime
 from pathlib import Path
+import pygame
 
+pygame.init()
 
 camera_width = 1920
 camera_height = 1080
@@ -15,6 +17,8 @@ imgwindow = "fotobooth"
 
 event_name = "weddding" #defined the name where the images are stored
 frame_name = "frame.png" #the name of the custom frame
+
+screen = pygame.display.set_mode((screen_width, screen_height),pygame.FULLSCREEN)
 
 script_path = Path(__file__).parent
 storage_path = script_path/event_name
@@ -35,35 +39,39 @@ alpha = ((alpha_ / 255)-1)*-1
 
 flashimage = np.ones_like(fotoframe)*200
 
-
-cv2.namedWindow(imgwindow, cv2.WINDOW_NORMAL)
-cv2.setWindowProperty(imgwindow,  cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-
 running = True
 
 while running:
     retval, image = camera.read()
-    # fotoframe[100:2260,100:3940] = image
     canvas = np.zeros_like(fotoframe)
     canvas[fotoframe_boader:(camera_height + fotoframe_boader),fotoframe_boader:(camera_width + fotoframe_boader)] = image
     canvas[:,:,0] = canvas[:,:,0] * alpha
     canvas[:, :, 1] = canvas[:, :, 1] * alpha
     canvas[:, :, 2] = canvas[:, :, 2] * alpha
     canvas = canvas + fotoframe
-    canvas = cv2.resize(canvas, (screen_width, screen_height))
-    cv2.imshow(imgwindow, canvas)
-    k = cv2.waitKey(33)
-    if k == 32:
-        # Capture a photo
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filename = storage_path/f"{timestamp}.jpg"
-        cv2.imwrite(filename.__str__(), canvas)
-        cv2.imshow(imgwindow, flashimage )
-        cv2.waitKey(60)
-    elif k == 27:
-        running = False
-    #else:
-        #print(k)
-# Clean up
 
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                # Capture a photo
+                timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                filename = storage_path / f"{timestamp}.jpg"
+                cv2.imwrite(filename.__str__(), canvas)
+                screen.fill((255, 255, 255))  # Set background to black
+                pygame.display.flip()
+                time.sleep(0.3)
+            elif event.key == pygame.K_ESCAPE:
+                running = False
+
+    frame = cv2.resize(canvas, (screen_width, screen_height))
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    frame = np.rot90(frame)
+    frame = np.flipud(frame)
+    frame = pygame.surfarray.make_surface(frame)
+    screen.blit(frame, (0, 0))
+    pygame.display.flip()
+
+# Clean up
+pygame.quit()
 cv2.destroyAllWindows()
